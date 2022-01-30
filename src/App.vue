37adb1db2ea482a20c665aa03b11e4eeb48b90bc
yomoberry <4morgan.9loops.leidbert@gmail.com>
2022-01-30 10:47:44 +0000

    first

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <button @click="createNewTodo">Add Todo</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{todo.name}} - {{todo.description}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import API, {  graphqlOperation } from '@aws-amplify/api';
+// eslint-disable-next-line
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from './graphql/queries'
+window.LOG_LEVEL = 'VERBOSE';
+export default {
+    name: 'app',
+    data(){
+        return {
+            todos: []
+        }
+    },
+    methods :{
+        async createNewTodo(){
+            const todo = { name: "Todo Title" , description: "あれをやる" + Date()}
+            await API.graphql(graphqlOperation(createTodo, { input: todo }))
+        },
+        async getData(){
+            const todoData = await API.graphql(graphqlOperation(listTodos))
+            this.todos.push(...this.todos, ...todoData.data.listTodos.items);
+        }
+    },
+    created(){
+        this.getData()
+    }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import App from './App.vue'
+import API from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+import awsconfig from './aws-exports';
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
